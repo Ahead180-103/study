444a6069e0448f103d7f4b8fce8e740dc24e9f1a
Mr.Gao <ahead_888@163.com>
2018-06-29 09:31:19 +0800

    add

--- shell/shelldb/scp.sh
+++ shell/shelldb/scp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "请输入文件或目录:  "  i
-[ $# -eq 0 ] &&  exit
-[ -f  $i ] && exit
-[ -d  $i ] && exit
+[ $# -eq 0 ] &&  echo  "请输入文件或目录:  " && exit
+#[ -e $i ] && exit
+#[ -f  $i ] && exit
+#[ -d  $i ] && exit
 expect <<EOF
-spawn  scp -r $i admin@www.svnup.cn:/home/admin/  
-expect "password:"  {send "gauyao1125\n"}
+spawn  scp -r $1 admin@www.tarena.cn:/home/admin/  
+expect "password:"  {send "123\n"}
 expect "#"  {send "exit\n"}
 EOF
